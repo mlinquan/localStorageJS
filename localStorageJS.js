1502cf57add38104fe990116bc05bbc83218d334
LinQuan <linquan@hexindai.com>
2015-04-13 14:46:07 +0800

    localStorage key prefix 'lsI_' and remove useless localStorage item

--- localStorageJS.js
+++ localStorageJS.js
@@ -3,7 +3,7 @@
  * https://github.com/mlinquan/localStorageJS
  *
  * @version
- * 0.0.6 (April 13, 2015)
+ * 0.0.7 (April 13, 2015)
  *
  * @copyright
  * Copyright (C) 2013 LinQuan.
@@ -14,7 +14,7 @@
 
  function localStorageJS(a, options, localStorageJSTag) {
     var qq = 0;
-    var lsJ = lsCount = a.length, lsI=0, lsList = {}, lsOnload = [], lsPanding = {}, lsAllready = 0, second = new Date().getTime()/1000,
+    var lsJ = lsCount = a.length, lsI=0, lsQ = {}, lsList = {}, lsOnload = [], lsPanding = {}, lsAllready = 0, second = new Date().getTime()/1000,
     head = document.head || document.getElementsByTagName('head')[0],
     localstorageable = (typeof window.localStorage != 'undefined'),
     localhost = location.hostname,
@@ -101,6 +101,7 @@
         return element;
     },
     doStorage = function(name, data) {
+        name = (name != "localStorageJS") && "lsI_" + name || name;
         if(localstorageable) {
             localStorage.setItem(name, JSON.stringify(data));
         }
@@ -153,6 +154,26 @@
                 };
                 xhr.open('get', obj.url);
                 xhr.send(null);
+            }
+        }
+    },
+    onloadFun = function() {
+        if(localstorageable) {
+            if(!config.debug && localstorageable && lsOnload.length) {
+                for(var g=0;g<lsOnload.length;g++) {
+                    var scTmp = localStorage.getItem("lsI_" + lsOnload[g].name),
+                    thatTmp;
+                    try{
+                        thatTmp = JSON.parse(scTmp);
+                    } catch(e) {
+                    }
+                    if(!thatTmp || (thatTmp && thatTmp.url != lsOnload[g].url)) {
+                        doGet(lsOnload[g]);
+                    }
+                }
+            }
+            for(x in localStorage) {
+                /^lsI_/.test(x) && !lsQ[x.replace("lsI_", "")] && localStorage.removeItem(x);
             }
         }
     },
@@ -219,7 +240,7 @@
                     (lsList[q].callback && typeof lsList[q].callback === 'function') && lsList[q].callback();
                     lsCount--;
                 }
-                if(lsList[q].type == 'js' && (lsList[q].removeSelf || config.removeAll) && lsList[q].lsStatus != "error") {
+                if(!config.debug && lsList[q].type == 'js' && (lsList[q].removeSelf || config.removeAll) && lsList[q].lsStatus != "error") {
                     head.removeChild(lsList[q].el);
                     lsList[q].removed = true;
                 }
@@ -227,6 +248,7 @@
             if(config.allready && typeof config.allready == "function") {
                 config.allready();
             }
+            //onloadFun();
             //head.removeChild(localStorageJSTag);
         }
     };
@@ -240,6 +262,7 @@
             lsCount--;
             continue;
         }
+        lsQ[a[i].name] = a[i];
         if(a[i].router && a[i].router.path) {
             var tmp_reg;
             try{
@@ -263,8 +286,8 @@
         a[i].islocal = (tmp_a.hostname == location.hostname);
         a[i].relurl = (!a[i].islocal && config.corsProxy) ? config.corsProxy + tmp_url : tmp_url;
         a[i].type = elementType(a[i].url);
-        if(!config.debug && localstorageable && localStorage.getItem(a[i].name)) {
-            var scTmp = localStorage.getItem(a[i].name),
+        if(!config.debug && localstorageable && localStorage.getItem("lsI_" + a[i].name)) {
+            var scTmp = localStorage.getItem("lsI_" + a[i].name),
             thatTmp;
             try{
                 thatTmp = JSON.parse(scTmp);
@@ -286,21 +309,11 @@
     for(x in lsList) {
         release(x);
     }
-    window.onload = function() {
-        if(!config.debug && localstorageable && lsOnload.length) {
-            for(var g=0;g<lsOnload.length;g++) {
-                var scTmp = localStorage.getItem(lsOnload[g].name),
-                thatTmp;
-                try{
-                    thatTmp = JSON.parse(scTmp);
-                } catch(e) {
-                }
-                if(!thatTmp || (thatTmp && thatTmp.url != lsOnload[g].url)) {
-                    doGet(lsOnload[g]);
-                }
-            }
-        }
-    };
+    if(window.addEventListener) {
+        window.addEventListener('load',onloadFun,false);
+    } else {
+        window.attachEvent('onload',onloadFun);
+    }
     if(!config.debug && (localstorageable && !localStorage.getItem('localStorageJS'))) {
         var lsTmp = {
             version: lsJS.version,
